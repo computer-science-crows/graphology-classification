42e94849d1873dac9383453974a28b5e38ce286d
KevinTD15 <talaveradiazkevin1999@gmail.com>
2023-05-01 21:56:21 -0500

    fixed problem with slicing

--- Dataset/scrapper.py
+++ Dataset/scrapper.py
@@ -23,7 +23,7 @@ def get_text_from_html(text, dataset):
 
 def findall_score(soap, dataset):
     for i in dataset:
-        start = soap.find('score', 0, len(soap))
+        start = soap.find('score:', 0, len(soap))
         if(start == -1):
             break
         end = soap.find('-', start, len(soap))
@@ -42,6 +42,7 @@ def scrapper(urls):
         url = urls[i]
         response = requests.get(url)
         dataset = get_text_from_html(response.text, dataset)
+        print(i)
     return dataset
 
 def generate_excel(urls, rute):
@@ -50,9 +51,8 @@ def generate_excel(urls, rute):
     df.to_excel(xlsx, 'Big Five Data', index=True)
     xlsx.close()
 
-urls = ['https://bigfive-test.com/result/64278a5b2da61900099f4840',
-        'https://bigfive-test.com/result/642626e77033800008bde884',
-        'https://bigfive-test.com/result/6424eb5bfa524d0008d3bbcf',
+#Alejandra
+urls = [
         'https://bigfive-test.com/result/6431f77e0e940e000826b0db',
         'https://bigfive-test.com/result/64336f260fdce60008273ed1',
         'https://bigfive-test.com/result/643374360fdce60008273ef7',
@@ -68,6 +68,26 @@ urls = ['https://bigfive-test.com/result/64278a5b2da61900099f4840',
         'https://bigfive-test.com/result/64489f12aae96d000880dcda',
         'https://bigfive-test.com/result/644894888fc17a0008362da4']
 
-rute = 'D:/UniVerSiDaD/IV Ano/Machine Learning/Project/Dataset/big_five.xlsx'
+#Kevin
+# urls = ['https://bigfive-test.com/result/64278a5b2da61900099f4840',
+#         'https://bigfive-test.com/result/642626e77033800008bde884',
+#         'https://bigfive-test.com/result/6424eb5bfa524d0008d3bbcf',
+#         'https://bigfive-test.com/result/642461e5d8db9800088449ea',
+#         'https://bigfive-test.com/result/6424a37b66adcd0008402e89',
+#         'https://bigfive-test.com/result/6424ba605008820008120208',
+#         'https://bigfive-test.com/result/6424b3c350088200081201cd',
+#         'https://bigfive-test.com/result/64247211b85f700009620881',
+#         'https://bigfive-test.com/result/6424776bb85f7000096208e7',
+#         'https://bigfive-test.com/result/64246696d8db980008844a25',
+#         'https://bigfive-test.com/result/6423951843f41200087e9ccc',
+#         'https://bigfive-test.com/result/6423924a43f41200087e9cad',
+#         'https://bigfive-test.com/result/642389c343f41200087e9c4d',
+#         'https://bigfive-test.com/result/642376c56b9c700008b6ad66',
+#         'https://bigfive-test.com/result/642374af6b9c700008b6ad50',
+#         'https://bigfive-test.com/result/64236db66b9c700008b6acf8',
+#         'https://bigfive-test.com/result/642367da6b9c700008b6acc7'    
+# ]
+
+rute = 'D:/UniVerSiDaD/IV Ano/Machine Learning/Project/Graphology classification/graphology-classification/Dataset/big_five.xlsx'
 
 generate_excel(urls, rute)
